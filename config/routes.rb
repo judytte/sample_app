b851bb5be161d896cf6161345a9110b293920692
judit <judit0240@gmail.com>
2018-03-06 22:11:54 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_p_ages/home'
+  get 'static_pages/home'
 
-  get 'static_p_ages/help'
+  get 'static_pages/help'
+
+  get 'static_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
